7b997c2ded094e31b84916d02494f55f72e472af
minus9d <minus9d@gmail.com>
2014-02-08 20:40:54 +0900

    initial commit.

---
+++ dive_into_python3/ch2.py
@@ -0,0 +1,127 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+
+a = ['a', 3, 'd']
+a.append('added')
+print (a)
+print (a[0])
+print (a[-1])
+# error
+# print (a[-10])
+
+# OK
+a.insert(20, 'sss')
+print (a)
+
+a.extend([10, True, 2.2])
+print (a)
+
+# Probably this is not what you want
+a.append([10, True, 2.2])
+print (a)
+
+
+print (a.count(2.2))
+print (a.count(2.9))
+print (True in a)
+print (False in a)
+
+print (a.index(2.2))
+
+# error
+# print (a.index(2.9))
+
+print (a)
+del a[1]
+print (a)
+
+b = [3, 3, 3, 3, 3]
+# first 3 is removed
+b.remove(3)
+print (b)
+
+
+print (a)
+popped = a.pop(4)
+print(popped)
+print (a)
+
+
+c = ("a", "b")
+print(c)
+# error
+# c[0] = "d"
+
+# make a tuple from a list
+d = tuple(a)
+print (d)
+
+# make a list from a tuple
+e = list(d)
+print (e)
+
+# tuple with only one member
+f = (1,)
+
+# 0, 1, 2
+(MON, TUE, WED) = range(3)
+print (MON)
+
+
+# set
+g = {2, 3, 5, 7}
+h = {2, 4, 6, 8}
+print (g)
+
+# this is not a set but a dictionary
+i = {}
+print (type(i))
+
+# this is a set
+i = set()
+print (type(i))
+
+g.update({10, 20})
+g.add(77)
+print (g)
+
+# safe
+g.discard(77)
+g.discard(77)
+g.discard(77)
+print (g)
+
+g.remove(2)
+# error
+# g.remove(2)
+print (g)
+
+
+
+# ややこしい型
+
+def show_type(a):
+    print( type(a) )
+    
+
+# 整数型
+show_type( 3 )
+show_type( (3) )
+
+# リスト
+show_type( [] )
+show_type( [1, 2, 3] )
+
+# タプル
+show_type( () )
+show_type( (3,) )
+show_type( (3, 4) )
+
+# 辞書
+show_type( {} )
+show_type( {"key1": 1, "key2": 2} )
+
+# 集合
+show_type( set() )
+show_type( {2, 3, 5} )
---
+++ dive_into_python3/ch3.py
@@ -0,0 +1,59 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+import time
+import humansize #自作ファイルを読み込み
+
+print(os.getcwd()) # cwd = current working directory
+os.chdir('C:\\')
+print(os.getcwd()) # cwd = current working directory
+
+os.chdir('C:\\Users')
+print(os.getcwd()) # cwd = current working directory
+
+# error
+#os.chdir('C:\\nowhere')
+
+path = os.path.join('/cygdrive/c', 'work')
+print(path)
+
+# ホームディレクトリ
+print(os.path.expanduser('~'))
+
+# ディレクトリとファイル名に分ける
+(dirname, filename) = os.path.split(path)
+print (dirname, filename)
+
+# 拡張子とそれ以外に分ける
+(shortname, extension) = os.path.splitext('something.txt')
+print (shortname, extension)
+
+# ファイル情報の取得
+os.chdir(os.path.expanduser('~'))
+metadata = os.stat('.zshrc')
+print (metadata.st_mtime)  # ファイルの最終更新日
+print (time.localtime(metadata.st_mtime)) # 読みやすく
+print (metadata.st_size) # ファイルのサイズ
+print (humansize.approximate_size(metadata.st_size)) # 読みやすく
+
+# 絶対パスを取得
+print (os.path.realpath('.zshrc'))
+
+# リスト内包表記
+a = [elem * 2 for elem in range(1, 100) if elem % 3 == 0]
+print (a)
+
+# 辞書内包表記
+b = {str: len(str) for str in ["Monday", "Tuesday", "Wednesday"]}
+print (b)
+c = list(b.keys())
+print (c)
+b_reverse = {value: key for (key, value) in b.items()}
+print (b_reverse)
+
+# 集合内包表記
+d = {item * 2 for item in range(1,10)}
+print (d)
+
