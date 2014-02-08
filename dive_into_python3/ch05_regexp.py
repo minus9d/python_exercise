971854d2131901697edb5cd7c1c54784640d96ca
minus9d <minus9d@gmail.com>
2014-02-08 23:52:08 +0900

    added ch05.

---
+++ dive_into_python3/ch05_regexp.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+s = 'abcdefghi'
+print(s.replace('ghi', 'ZZZ'))
+print(re.sub('ghi$', 'ZZZ', s))
+
+# 世紀表現
+pattern = r'<a href="(.*?)">'
+s = '<a href="a.html">aaa</a> <a href="b.html">bbb</b>'
+match = re.findall(pattern, s)
+print (match)
+
+# 冗長な正規表現
+# 半角スペースが使えなくなるので注意
+pattern = '''
+<a\s*href="
+(.*?)  # url
+">
+'''
+s = '<a href="a.html">aaa</a> <a href="b.html">bbb</b>'
+match = re.findall(pattern, s, re.VERBOSE)
+print (match)
+
+
+
+
+
