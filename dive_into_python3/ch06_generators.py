6ed0b773fb7ae85b834925db7988b00cadd657f3
minus9d <minus9d@gmail.com>
2014-02-09 15:27:18 +0900

    ジェネレータを使って書き直し

--- dive_into_python3/ch06_generators.py
+++ dive_into_python3/ch06_generators.py
@@ -10,21 +10,21 @@ def build_match_and_apply_functions(pattern, search, replace):
         return re.sub(search, replace, word)
     return (matches_rule, apply_rule)     
 
-rules = []
-# withによりコンテクストを作り、自動でファイルを閉じることが出来る
-with open('ch06_rules.txt', encoding='utf-8') as pattern_file:
-    for line in pattern_file:
-        print ("[", line, "]")
-        # 空白文字で分割
-        # 末尾の改行は含まれないらしい
-        pattern, search, replace = line.split(None, 3)
-        print ("replace = ", replace, "]")
-        rules.append(build_match_and_apply_functions(pattern, search, replace))
+def rules(rules_filename):
+    # withによりコンテクストを作り、自動でファイルを閉じることができる
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            # 空白文字で分割
+            # 末尾の改行は含まれないらしい
+            pattern, search, replace = line.split(None, 3)
+            yield build_match_and_apply_functions(pattern, search, replace)
 
-def plural(noun):
-    for matches_rule, apply_rule in rules:
+def plural(noun, rules_filename = 'ch06_rules.txt'):
+    for matches_rule, apply_rule in rules(rules_filename):
         if matches_rule(noun):
             return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
+
 # 1
 print (plural('index'))
 # 2
