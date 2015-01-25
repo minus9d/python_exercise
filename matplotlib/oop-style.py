bb3fcf1a5c680c66906659df1f19e6b92b8b7898
minus9d <minus9d@gmail.com>
2015-01-25 23:40:17 +0900

    matplotlibをオブジェクト指向らしく使う例を追加

--- matplotlib/oop-style.py
+++ matplotlib/oop-style.py
@@ -4,15 +4,17 @@
 # matplotlibをオブジェクト指向らしく使う
 # 参考: http://matplotlib.org/examples/pylab_examples/pythonic_matplotlib.html
 
-from pylab import figure, show
-from numpy import arange, sin, pi
+import matplotlib.pyplot as plt
+import numpy as np
 
-t = arange(0.0, 1.0, 0.01)
+t = np.arange(0.0, 1.0, 0.01)
 
-fig = figure(1)
+fig = plt.figure(1)
 
+# axはaxesのこと。線やテキストを加える矩形領域に相当。
+# figureの中に1個以上のaxが存在する。
 ax1 = fig.add_subplot(211)
-ax1.plot(t, sin(2*pi*t))
+ax1.plot(t, np.sin(2*np.pi*t))
 ax1.grid(True)
 ax1.set_ylim( (-2,2) )
 ax1.set_ylabel('1 Hz')
@@ -21,13 +23,12 @@ ax1.set_title('A sine wave or two')
 for label in ax1.get_xticklabels():
     label.set_color('r')
 
-
 ax2 = fig.add_subplot(212)
-ax2.plot(t, sin(2*2*pi*t))
+ax2.plot(t, np.sin(2*2*np.pi*t))
 ax2.grid(True)
 ax2.set_ylim( (-2,2) )
 l = ax2.set_xlabel('Hi mom')
 l.set_color('g')
 l.set_fontsize('large')
 
-show()
+plt.show()
