d90a487b014e6550aa3ee0b2245e9cfcf752dc79
minus9d <minus9d@gmail.com>
2015-03-23 23:30:01 +0900

    pyqt5 zetcode tutorial ch03を追加

---
+++ PyQt/PyQt5/zetcode_tutorial/ch03-04-review.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial 
+
+In this example, we create a bit
+more complicated window layout using
+the QGridLayout manager. 
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: January 2015
+"""
+
+import sys
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, 
+    QTextEdit, QGridLayout, QApplication)
+
+
+class Example(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):
+        
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        # テキストボックス
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10) # Widget間のスペース
+
+        # 最上段
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        # 中段
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        # 最下段
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)  # 高さは5段分
+        
+        self.setLayout(grid) 
+        
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')    
+        self.show()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
