f063d581990b97013482fb3678e2f0803300139c
minus9d <minus9d@gmail.com>
2015-03-24 23:28:51 +0900

    pyqt5 zetcode tutorial ch04を追加

---
+++ PyQt/PyQt5/zetcode_tutorial/ch04-04-emit-signals.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial 
+
+In this example, we show how to emit a
+signal. 
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: January 2015
+"""
+
+import sys
+from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Aaa():
+    cc = 100
+
+
+# QObjectから作られたオブジェクトはシグナルを放出できる
+class Communicate(QObject):
+    # class attributeとしてSignalが生成される
+    closeApp = pyqtSignal() 
+    
+
+class Example(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        # 自作したcloseAppシグナルが、QMainWindowのclose()に接続される
+        self.c = Communicate()
+        self.c.closeApp.connect(self.close)       
+        
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Emit signal')
+        self.show()
+        
+        
+    def mousePressEvent(self, event):
+        # マウスをクリックするとシグナルが送出されてウィンドウが閉じる
+        self.c.closeApp.emit()
+        
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
