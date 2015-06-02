8c43eee63edc49adaa192c3ed9c3f75a165d3bf0
minus9d <minus9d@gmail.com>
2015-06-02 23:53:59 +0900

    flake8の出力に従い修正

--- unittest/doctest_sample.py
+++ unittest/doctest_sample.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 # python -m doctest doctest_sample.py とすると、簡易的なユニットテストになる
 # 以下ではわざと一つ間違えている
 def add_to_n(n):
@@ -14,10 +15,9 @@ def add_to_n(n):
     """
     return n * (n+1) // 2
 
+
 def main():
     print(add_to_n(10))
 
 if __name__ == '__main__':
     main()
-
-    
